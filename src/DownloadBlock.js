4589c7049e7f141541a3905dbe25e9548cd72d9d
Satvir Sandhu <356Sandhu@gmail.com>
2020-06-20 15:15:52 -0400

    add DownloadBlock component

---
+++ src/DownloadBlock.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DownloadBlock = () => {
+  return (
+    <div className="downloadBlock lightBlock">
+      <p>Android</p>
+    </div>
+  );
+};
+
+export default DownloadBlock;
