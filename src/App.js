b4585a25955cc956b818781920e186b457aadfa4
Satvir Sandhu <356Sandhu@gmail.com>
2020-08-07 12:18:06 -0400

    remove issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react";
 import firebase from "./firebase";
+
 import Card from "./Card";
 import Filter from "./Filter";
 import ThemeSwitcher from "./ThemeSwitcher";
